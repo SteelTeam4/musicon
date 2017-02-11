af82a2feb060dc7788fbfcf11673717c02976b89
saatvikshah1994 <saatvikshah1994@gmail.com>
2017-02-11 07:02:26 -0500

    improved spotify usage

--- app/src/main/java/com/invaders/musicon/musicon/MainActivity.java
+++ app/src/main/java/com/invaders/musicon/musicon/MainActivity.java
@@ -1,7 +1,10 @@
 package com.invaders.musicon.musicon;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 
 import android.app.Activity;
@@ -10,17 +13,25 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
 import com.spotify.sdk.android.authentication.AuthenticationResponse;
 import com.spotify.sdk.android.player.Config;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Connectivity;
 import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.Metadata;
+import com.spotify.sdk.android.player.PlaybackState;
 import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerEvent;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity implements
         SpotifyPlayer.NotificationCallback, ConnectionStateCallback, View.OnClickListener {
@@ -32,7 +43,32 @@ public class MainActivity extends Activity implements
     private static final int REQUEST_CODE = 1337;
 
     private static final String TAG = "MainActivityTAG";
-    private Player mPlayer;
+    private SpotifyPlayer mPlayer;
+    private PlaybackState mCurrentPlaybackState;
+
+    private static final List<String> TEST_ALBUM_TRACKS = Arrays.asList(
+            "spotify:track:0MllcbBCVEvDJ1jQBbfnp0",
+            "spotify:track:0Cim88rt6HG172g8yLSYS5",
+            "spotify:track:1mq49Np3oRodGq0HM4h3C9",
+            "spotify:track:7djK8tEU7rmzqU73aKBBEG",
+            "spotify:track:0CSBgxYPRCSd1Qzo1EzAN3"
+    );
+
+    private BroadcastReceiver mNetworkStateReceiver;
+    private Metadata mMetadata;
+    private int songIdx;
+
+    private final Player.OperationCallback mOperationCallback = new Player.OperationCallback() {
+        @Override
+        public void onSuccess() {
+            Log.d(TAG, "OK!");
+        }
+
+        @Override
+        public void onError(Error error) {
+            Log.d( TAG, "ERROR:" + error );
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,10 +93,11 @@ public class MainActivity extends Activity implements
         final int id = v.getId();
         switch ( id ) {
             case R.id.bSpotifyLogin:
-                doLogin();
+                onLoginButtonClicked();
                 break;
             case R.id.bPlay:
                 Log.d(TAG,"Clicked Play");
+                onPlayButtonClicked();
                 break;
             case R.id.bPause:
                 Log.d(TAG,"Clicked Pause");
@@ -71,39 +108,93 @@ public class MainActivity extends Activity implements
         }
     }
 
-    protected void doLogin() {
-        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
-                AuthenticationResponse.Type.TOKEN,
-                REDIRECT_URI);
-        builder.setScopes(new String[]{"user-read-private", "streaming"});
-        AuthenticationRequest request = builder.build();
+
+    private void openLoginWindow() {
+        final AuthenticationRequest request = new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
+                .setScopes(new String[]{"user-read-private", "playlist-read", "playlist-read-private", "streaming"})
+                .build();
 
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    private void onAuthenticationComplete(AuthenticationResponse authResponse) {
+        // Once we have obtained an authorization token, we can proceed with creating a Player.
+        if (mPlayer == null) {
+            Config playerConfig = new Config(getApplicationContext(), authResponse.getAccessToken(), CLIENT_ID);
+            // Since the Player is a static singleton owned by the Spotify class, we pass "this" as
+            // the second argument in order to refcount it properly. Note that the method
+            // Spotify.destroyPlayer() also takes an Object argument, which must be the same as the
+            // one passed in here. If you pass different instances to Spotify.getPlayer() and
+            // Spotify.destroyPlayer(), that will definitely result in resource leaks.
+            mPlayer = Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                @Override
+                public void onInitialized(SpotifyPlayer player) {
+                    mPlayer.addNotificationCallback(MainActivity.this);
+                    mPlayer.addConnectionStateCallback(MainActivity.this);
+                }
+
+                @Override
+                public void onError(Throwable error) {
+                    Log.d(TAG,"Error in initialization: " + error.getMessage());
+                }
+            });
+        } else {
+            mPlayer.login(authResponse.getAccessToken());
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
+
         // Check if result comes from the correct activity
         if (requestCode == REQUEST_CODE) {
             AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
-            if (response.getType() == AuthenticationResponse.Type.TOKEN) {
-                Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
-                Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
-                    @Override
-                    public void onInitialized(SpotifyPlayer spotifyPlayer) {
-                        mPlayer = spotifyPlayer;
-                        mPlayer.addConnectionStateCallback(MainActivity.this);
-                        mPlayer.addNotificationCallback(MainActivity.this);
-                    }
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    onAuthenticationComplete(response);
+                    break;
 
-                    @Override
-                    public void onError(Throwable throwable) {
-                        Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
-                    }
-                });
+                // Auth flow returned an error
+                case ERROR:
+                    Log.d(TAG,"Auth error: " + response.getError());
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    Log.d(TAG,"Auth result: " + response.getType());
             }
         }
+    }
+
+    private boolean isLoggedIn() {
+        return mPlayer != null && mPlayer.isLoggedIn();
+    }
+
+    public void onLoginButtonClicked() {
+        if (!isLoggedIn()) {
+            Log.d(TAG,"Logging in");
+            openLoginWindow();
+        } else {
+            mPlayer.logout();
+        }
+    }
+
+    public void onPauseButtonClicked() {
+        mPlayer.pause(mOperationCallback);
+    }
+
+    public void onPlayButtonClicked() {
+        Log.d(TAG, "Clicked Play");
+        Log.d(TAG,""+songIdx);
+        mPlayer.refreshCache();
+        mPlayer.playUri(mOperationCallback,TEST_ALBUM_TRACKS.get(songIdx++),0,0);
+    }
+
+
+    public void onSkipToNextButtonClicked() {
+        mPlayer.skipToNext(mOperationCallback);
     }
 
     @Override
@@ -113,16 +204,79 @@ public class MainActivity extends Activity implements
     }
 
     @Override
-    public void onPlaybackEvent(PlayerEvent playerEvent) {
-        Log.d("MainActivity", "Playback event received: " + playerEvent.name());
-        switch (playerEvent) {
-            // Handle event type as necessary
-            default:
-                break;
+    public void onPlaybackEvent(PlayerEvent event) {
+        // Remember kids, always use the English locale when changing case for non-UI strings!
+        // Otherwise you'll end up with mysterious errors when running in the Turkish locale.
+        // See: http://java.sys-con.com/node/46241
+        mCurrentPlaybackState = mPlayer.getPlaybackState();
+        mMetadata = mPlayer.getMetadata();
+        Log.i(TAG, "Player state: " + mCurrentPlaybackState);
+        Log.i(TAG, "Metadata: " + mMetadata);
+        if ( event == PlayerEvent.kSpPlaybackNotifyAudioDeliveryDone ) {
+            Log.d( TAG, "Song just got over :(" );
+            if ( songIdx >= TEST_ALBUM_TRACKS.size() ) {
+                songIdx = 0;
+                mPlayer.refreshCache();
+                Log.d(TAG,""+songIdx);
+            } else {
+                Log.d(TAG,""+songIdx);
+                mPlayer.playUri(mOperationCallback,TEST_ALBUM_TRACKS.get(songIdx++),0,0);
+            }
         }
     }
 
+    private Connectivity getNetworkConnectivity(Context context) {
+        ConnectivityManager connectivityManager;
+        connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+        if (activeNetwork != null && activeNetwork.isConnected()) {
+            return Connectivity.fromNetworkType(activeNetwork.getType());
+        } else {
+            return Connectivity.OFFLINE;
+        }
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Set up the broadcast receiver for network events. Note that we also unregister
+        // this receiver again in onPause().
+        mNetworkStateReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (mPlayer != null) {
+                    Connectivity connectivity = getNetworkConnectivity(getBaseContext());
+                    Log.d(TAG, "Network state changed: " + connectivity.toString());
+                    mPlayer.setConnectivityStatus(mOperationCallback, connectivity);
+                }
+            }
+        };
+
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(mNetworkStateReceiver, filter);
+
+        if (mPlayer != null) {
+            mPlayer.addNotificationCallback(MainActivity.this);
+            mPlayer.addConnectionStateCallback(MainActivity.this);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mNetworkStateReceiver);
+
+        // Note that calling Spotify.destroyPlayer() will also remove any callbacks on whatever
+        // instance was passed as the refcounted owner. So in the case of this particular example,
+        // it's not strictly necessary to call these methods, however it is generally good practice
+        // and also will prevent your application from doing extra work in the background when
+        // paused.
+        if (mPlayer != null) {
+            mPlayer.removeNotificationCallback(MainActivity.this);
+            mPlayer.removeConnectionStateCallback(MainActivity.this);
+        }
+    }
 
     @Override
     public void onPlaybackError(Error error) {
@@ -137,7 +291,6 @@ public class MainActivity extends Activity implements
     @Override
     public void onLoggedIn() {
         Log.d("MainActivity", "User logged in");
-        mPlayer.playUri(null, "spotify:track:2TpxZ7JUBn3uw46aR7qd6V", 0, 0);
     }
 
     @Override
